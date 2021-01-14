b9c9b2f6568652368146a3f676a7bb9e8de16612
suka paus <182410102006@students.unej.ac.id>
2021-01-14 22:55:17 +0700

    🚀 set to heroku

--- db.js
+++ db.js
@@ -1,9 +1,9 @@
 const Pool = require('pg').Pool
 const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'db_tictactoe',
-  password: 'Senollol123',
+  user: 'xudfybtichiljh',
+  host: 'ec2-34-225-82-212.compute-1.amazonaws.com',
+  database: 'dfn92igfsgk11o',
+  password: '1571bbd895943aa4d2e8872528521d630b59dabc8d34dfb96248a414cd877e9e',
   port: 5432,
 })
 
